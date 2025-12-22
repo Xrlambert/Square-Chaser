cf034025503322ba55d5d4705cc2fa422a65ccf4
Xrlambert <xavierlambertt@gmail.com>
2025-12-22 15:41:37 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //Key States
+        bool UpDown, DownDown, RightDown, LeftDown;
+
+        //Position Variables
+        float x = 0;
+        float y = 0;
+
+        //Fwd/backward Speed
+        float speed = 0;
+
+        //Friction
+        float friction = 0.09f;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    UpDown = false;
+                    break;
+                case Keys.Down:
+                    DownDown = false;
+                    break;
+                case Keys.Left:
+                    LeftDown = false;
+                    break;
+                case Keys.Right:
+                    RightDown = false;
+                    break;
+            }
+            //if (e.KeyCode == Keys.Up) UpDown = true;
+            //if (e.KeyCode == Keys.Down) DownDown = true;
+            //if (e.KeyCode == Keys.Left) LeftDown = true;
+            //if (e.KeyCode == Keys.Right) RightDown = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    UpDown = true;
+                    break;
+                case Keys.Down:
+                    DownDown = true;
+                    break;
+                case Keys.Left:
+                    LeftDown = true;
+                    break;
+                case Keys.Right:
+                    RightDown = true;
+                    break;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            
+            // Move origin to vehicle center.
+            g.TranslateTransform(x, y);
+        }
+    }
+}
